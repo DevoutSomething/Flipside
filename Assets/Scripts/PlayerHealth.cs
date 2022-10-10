402578bc9fae59e87edd54ce58787b23f02493bc
Archer Fox <archer.l.fox@gmail.com>
2022-10-10 08:38:40 -0700

    goofy

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int Health = 1;
+    public int TempHealth = 1;
+    
+    public Animator playerAnimator;
+    public float deathTimer;
+    private float deathTimerTEMP;
+    public bool death;
+    public Vector2 respawnPoint;
+    private void Start()
     {
-        
+        playerAnimator = GetComponentInChildren<Animator>();
+        deathTimerTEMP = deathTimer;
+        respawnPoint = gameObject.transform.position;
+    }
+    private void Update()
+    {
+        if(TempHealth <= 0 || death == true)
+        {
+            death = true;
+            StartDeathTimer();
+            playerAnimator.SetBool("Death", true);
+            playerAnimator.SetBool("Run", false);
+            playerAnimator.SetBool("jump", false);
+
+
+        }
+        if (death && deathTimerTEMP >= 0)
+        {
+            deathTimerTEMP -= Time.fixedDeltaTime;
+        }
+        if(deathTimerTEMP <= 0 && death)
+        {
+            RespawnPlayer();
+        }
+    }
+    void StartDeathTimer()
+    {
+        death = true;
+        deathTimerTEMP = deathTimer;
     }
 
-    // Update is called once per frame
-    void Update()
+    void RespawnPlayer()
     {
-        
+        death = false;
+        playerAnimator.SetBool("Death", false);
+        gameObject.transform.position = respawnPoint;
+        TempHealth = 1;
     }
 }
